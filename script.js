579267a35c19b409c6c6721ff23ab701caad028b
rajibdas-it <rajibrad@gmail.com>
2022-09-14 21:20:50 +0600

    stop auto start by removing the () from start. i was added it

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start());
+startBtn.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
